888c43b805cc2019037fc97c201f2f3eb76ef4fa
Mehdi MOUSSA <moussa.mehdi@hotmail.com>
2026-08-20 16:55:04 +0200

    fix

--- api/db/benevolesRepository.go
+++ api/db/benevolesRepository.go
@@ -12,9 +12,6 @@ func CreateBenevole(benevole *models.Benevole, capacites []string) (int, error) 
 	}
 	defer tx.Rollback()
 
-	// statut_candidature n'est pas dans la liste des colonnes : ça laisse le
-	// DEFAULT 'en_attente' de la table s'appliquer, plutôt que de dépendre de
-	// benevole.StatutCandidature (qui vaut "" si l'appelant ne le fixe pas).
 	res, err := tx.Exec(
 		"INSERT INTO benevoles (email, password_hash, nom, prenom, telephone) VALUES (?, ?, ?, ?, ?)",
 		benevole.Email, benevole.PasswordHash, benevole.Nom, benevole.Prenom, benevole.Telephone,
